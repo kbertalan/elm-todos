18f996c938a7146760c250f92225457131e28ab0
Bertalan Kis <kbertalan@gmail.com>
2020-11-14 15:26:11 +0100

    counter

--- src/Pages/Top.elm
+++ src/Pages/Top.elm
@@ -1,9 +1,13 @@
 module Pages.Top exposing (Model, Msg, Params, page)
 
 import Element exposing (..)
+import Element.Border as Border exposing (..)
+import Element.Font as Font exposing (..)
+import Element.Input as Input exposing (button)
 import Spa.Document exposing (Document)
 import Spa.Page as Page exposing (Page)
 import Spa.Url exposing (Url)
+import Widget exposing (button)
 
 
 type alias Params =
@@ -11,26 +15,64 @@ type alias Params =
 
 
 type alias Model =
-    Url Params
+    { counter : Int }
 
 
-type alias Msg =
-    Never
+type Msg
+    = Increment
+    | Decrement
 
 
 page : Page Params Model Msg
 page =
-    Page.static
-        { view = view
+    Page.sandbox
+        { init = init
+        , update = update
+        , view = view
         }
+
+
+
+-- INIT
+
+
+init : Url Params -> Model
+init _ =
+    { counter = 0 }
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            { model | counter = model.counter + 1 }
+
+        Decrement ->
+            { model | counter = model.counter - 1 }
 
 
 
 -- VIEW
 
 
-view : Url Params -> Document Msg
-view { params } =
-    { title = "Homepage"
-    , body = [ text "Homepage" ]
+view : Model -> Document Msg
+view model =
+    { title = "Counter page"
+    , body =
+        [ row [ padding 10, spacing 5 ]
+            [ Widget.button black Decrement "-"
+            , el [ Font.bold ]
+                (text (String.fromInt model.counter))
+            , Widget.button black Increment "+"
+            ]
+        ]
     }
+
+
+black : Color
+black =
+    rgb255 0 0 0
