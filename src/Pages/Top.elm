68d90ed0805b92dfeb9a86e4c55a7f5eda6b291f
Bertalan Kis <kbertalan@gmail.com>
2020-11-14 23:11:59 +0100

    load todos from server

--- src/Pages/Top.elm
+++ src/Pages/Top.elm
@@ -5,6 +5,8 @@ import Element exposing (..)
 import Element.Border as Border
 import Element.Font as Font
 import Element.Input as Input
+import Http
+import Json.Decode as D
 import Spa.Document exposing (Document)
 import Spa.Page as Page exposing (Page)
 import Spa.Url exposing (Url)
@@ -14,20 +16,32 @@ type alias Params =
     ()
 
 
+type alias Todo =
+    { id : String
+    , description : String
+    , completed : Bool
+    }
+
+
 type alias Model =
-    { current : String }
+    { description : String
+    , todos : List Todo
+    }
 
 
 type Msg
-    = ChangeText String
+    = Edit String
+    | Create
+    | Loaded (Result Http.Error (List Todo))
 
 
 page : Page Params Model Msg
 page =
-    Page.sandbox
+    Page.element
         { init = init
         , update = update
         , view = view
+        , subscriptions = \_ -> Sub.none
         }
 
 
@@ -35,20 +49,43 @@ page =
 -- INIT
 
 
-init : Url Params -> Model
+init : Url Params -> ( Model, Cmd Msg )
 init _ =
-    { current = "" }
+    ( { description = ""
+      , todos = []
+      }
+    , load
+    )
 
 
 
 -- UPDATE
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ChangeText txt ->
-            { model | current = txt }
+        Edit desc ->
+            let
+                newModel =
+                    { model | description = desc }
+            in
+            ( newModel, Cmd.none )
+
+        Create ->
+            ( model, Cmd.none )
+
+        Loaded result ->
+            case result of
+                Ok data ->
+                    let
+                        newModel =
+                            { model | todos = data }
+                    in
+                    ( newModel, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
 
 
 
@@ -60,12 +97,39 @@ view model =
     { title = "Todos"
     , body =
         [ column [ padding 10, spacing 5, centerX, centerY ]
-            [ Input.text []
+            ([ Input.text
+                []
                 { label = Input.labelHidden "current"
-                , onChange = ChangeText
+                , onChange = Edit
                 , placeholder = Just <| Input.placeholder [] (text "todo")
-                , text = model.current
+                , text = model.description
                 }
-            ]
+             ]
+                ++ List.map
+                    (\todo ->
+                        text todo.description
+                    )
+                    model.todos
+            )
         ]
     }
+
+
+
+-- CMD
+
+
+load : Cmd Msg
+load =
+    Http.get
+        { url = "/api/todo"
+        , expect = Http.expectJson Loaded (D.list todoDecoder)
+        }
+
+
+todoDecoder : D.Decoder Todo
+todoDecoder =
+    D.map3 Todo
+        (D.field "id" D.string)
+        (D.field "description" D.string)
+        (D.field "completed" D.bool)
